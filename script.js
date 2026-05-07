81a080c72970e932fa4992910489308e0c07c98b
kylebaileyxl10-lang <kylebaileyxl10@gmail.com>
2026-05-07 06:54:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// Updated Clean Script for Ge-NET-ry Dash
-fetch('assets/project_data.xml')
-  .then(response => response.text())
-  .then(xmlData => {
-      // This is the correct command for your specific engine
-      try {
-          loadLevelLibrary(xmlData); 
-          console.log("Success: Your levels are ready!");
-      } catch (err) {
-          // If the first way fails, it tries the second way
-          Game.importSave(xmlData);
-      }
-  });
+// Clean Level List - Ready for Friday
+var myLevels = [
+    {
+        name: "Broken",
+        data: "PASTE_YOUR_LONG_LEVEL_STRING_HERE",
+        verified: true
+    }
+];
+
+// To see ALL levels: 
+// Click the document icon on the site and paste your project_data.xml
