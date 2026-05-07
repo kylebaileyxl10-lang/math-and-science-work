84554a03fd79bd212b1fe15fc91bc7d62d9d2312
kylebaileyxl10-lang <kylebaileyxl10@gmail.com>
2026-05-07 06:57:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-// Clean Level List - Ready for Friday
-var myLevels = [
-    {
-        name: "Broken",
-        data: "PASTE_YOUR_LONG_LEVEL_STRING_HERE",
-        verified: true
-    }
-];
-
-// To see ALL levels: 
-// Click the document icon on the site and paste your project_data.xml
+// Geometry Dash Web - Math & Science Work
+console.log("Game script loaded successfully.");
